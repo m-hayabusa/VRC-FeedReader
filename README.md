491613f186586eec7c1343e6dba39812e658f4bb
hayabusa <s.hashimoto@hs-sh.net>
2023-02-23 17:49:55 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,79 @@
+# nS videoParser (VRC_get_text_from_video)
+## これは
+VRChatで文字列と画像を動画から読み出すためのツールです
+[m-hayabusa/send_text_to_vrc](https://github.com/m-hayabusa/send_text_to_vrc) と組み合わせて使います
+
+
+### サーバー側: 
+```javascript
+import * as send_text_to_vrc from "send_text_to_vrc";
+
+const files = [];
+
+const table = new send_text_to_vrc.File("list", ["name", "comment"]);
+table.push(["エビフライ", "1つ"]);
+table.push(["卵", "1パック"]);
+files.push(table);
+
+const images = new send_text_to_vrc.Images("img");
+images.push("エビフライの画像", "./ebifly.png");
+images.push("卵の画像", "./egg.png");
+files.push(images);
+
+send_text_to_vrc.publish(files, "./kaimonolist.webm");
+// この後、生成された kaimonolist.webm をhttps経由でアクセスできるようにする必要があります
+```
+
+
+### U#側:
+1. このパッケージを導入
+    * おすすめ: `vpm add repo https://m-hayabusa.github.io/VRC_get_text_from_video/index.json` を実行して、VRChat CreatorCompanionから`nS videoParser`をプロジェクトに追加  
+      (`vpm`コマンドについては[CLI | VRChat Creator Companion: https://vcc.docs.vrchat.com/vpm/cli](https://vcc.docs.vrchat.com/vpm/cli)を参照)
+    * https://github.com/m-hayabusa/VRC_get_text_from_video/releases から最新の unitypackage をダウンロードして import
+2. Hierarchyを右クリック、 nsVideoParser を追加
+3. nsVideoParser についている v2sConfig.Url に、上記 kaimonolist.webm へのURLを入れておく
+
+```csharp
+using UnityEngine;
+using UdonSharp;
+using nekomimiStudio.video2String;
+
+public class kaimonolist : UdonSharpBehaviour
+{
+    public Video2Str video2Str; // nsVideoParser についている Video2Str をここに割りあてる
+
+    private bool done = false;
+    private Parser parser;
+    [SerializeField] private RenderTexture[] rTex;
+    
+    void Start(){
+        parser = video2Str.getParser();
+    }
+    
+    void Update()
+    {
+        if (!done)
+        {
+            Debug.Log($"progress: {video2Str.getDecodeProgress()}");
+
+            if (parser.isDone())
+            {
+                for (int i = 0; i < parser.getLength("list"); i++)
+                    Debug.Log($"{i}: {parser.getString("list", i, "name")}, {parser.getString("list", i, "comment")}");
+
+                for (int i = 0; i < parser.getLength("img"); i++) {
+                    Debug.Log(parser.getString("img", i, "filename"));
+                    video2Str.GetTexture(rTex[i], "img", i);
+                }
+
+                done = true;
+            }
+        }
+    }
+
+    public override void Interact()
+    {
+        video2Str.reload();
+    }
+}
+```
---
+++ Runtime/Script/feedReader.cs
@@ -0,0 +1,244 @@
+﻿using UnityEngine;
+using UdonXMLParser;
+using VRC.SDKBase;
+using VRC.SDK3.StringLoading;
+using UdonSharp;
+
+namespace nekomimiStudio.feedReader
+{
+    public class feedReader : UdonSharpBehaviour
+    {
+        private UdonXML udonXml;
+        public VRCUrl[] FeedURL;
+
+        private string[][][][] res;
+        /*
+            Result res = Feed[];
+            Feed = {
+                Header,
+                Entry[]
+            }
+
+            Header = {
+                Title[1],
+                SubTitle[1],
+                Summary[1],
+                Id[1],
+                Link[1],
+                Updated[1],
+                Rights[1],
+                AuthorName[1],
+                AuthorUri[1]
+            }
+
+            Entry = {
+                Title,
+                SubTitle,
+                Summary,
+                Id,
+                Link,
+                Updated,
+            }
+        */
+
+        private int loadingIttr = 0;
+        private bool done = true;
+
+        public void Start()
+        {
+            res = new string[FeedURL.Length][][][];
+            udonXml = this.GetComponent<UdonXML>();
+        }
+
+        public void Update()
+        {
+            if (done && loadingIttr < FeedURL.Length)
+            {
+                done = false;
+                VRCStringDownloader.LoadUrl(FeedURL[loadingIttr], (VRC.Udon.Common.Interfaces.IUdonEventReceiver)this);
+            }
+        }
+
+        public override void OnStringLoadSuccess(IVRCStringDownload stringDownload)
+        {
+            Debug.Log(stringDownload.Result);
+            var root = udonXml.LoadXml(stringDownload.Result);
+            var content = udonXml.GetChildNode(root, 1);
+            switch (udonXml.GetNodeName(content))
+            {
+                case "rdf:RDF":
+                    parseRSS1(loadingIttr, content);
+                    break;
+                case "rss":
+                    parseRSS2(loadingIttr, content);
+                    break;
+                case "feed":
+                    parseAtom(loadingIttr, content);
+                    break;
+            }
+        }
+
+        private string GetNodeValueByName(object data, string nodeName)
+        {
+            if (!udonXml.HasChildNodes(data)) return "";
+            var node = udonXml.GetChildNodeByName(data, nodeName);
+            if (node == null) return "";
+            return udonXml.GetNodeValue(node);
+        }
+        private void parseRSS1(int feedNum, object contentRoot)
+        {
+            var headerRoot = udonXml.GetChildNodeByName(contentRoot, "channel");
+
+            res[feedNum] = new string[2][][];
+            res[feedNum][0] = new string[10][];
+
+            res[feedNum][0][(int)feedHeader.Title] = new string[] { GetNodeValueByName(headerRoot, "title") };
+            res[feedNum][0][(int)feedHeader.SubTitle] = new string[] { GetNodeValueByName(headerRoot, "subtitle") };
+            res[feedNum][0][(int)feedHeader.Summary] = new string[] { GetNodeValueByName(headerRoot, "description") };
+            res[feedNum][0][(int)feedHeader.Updated] = new string[] { GetNodeValueByName(headerRoot, "dc:date") };
+            res[feedNum][0][(int)feedHeader.Link] = new string[] { GetNodeValueByName(headerRoot, "link") };
+            res[feedNum][0][(int)feedHeader.AuthorName] = new string[] { "" };
+            res[feedNum][0][(int)feedHeader.AuthorUri] = new string[] { "" };
+
+            string[][] entries = new string[udonXml.GetChildNodesCount(contentRoot)][];
+
+            int cnt = 0;
+            for (int i = 0; i < udonXml.GetChildNodesCount(contentRoot); i++)
+            {
+                var entry = udonXml.GetChildNode(contentRoot, i);
+                if (udonXml.GetNodeName(entry) == "item")
+                {
+                    entries[cnt] = new string[6];
+                    entries[cnt][(int)feedEntry.Title] = GetNodeValueByName(entry, "title");
+                    entries[cnt][(int)feedEntry.SubTitle] = GetNodeValueByName(entry, "subtitle");
+                    entries[cnt][(int)feedEntry.Link] = GetNodeValueByName(entry, "link");
+                    entries[cnt][(int)feedEntry.Summary] = GetNodeValueByName(entry, "description");
+                    entries[cnt][(int)feedEntry.Updated] = GetNodeValueByName(entry, "dc:date");
+                    cnt++;
+                }
+            }
+            res[feedNum][1] = new string[cnt][];
+
+            System.Array.Copy(entries, res[feedNum][1], cnt);
+
+            done = true;
+            loadingIttr++;
+        }
+
+        private void parseRSS2(int feedNum, object contentRoot)
+        {
+            contentRoot = udonXml.GetChildNodeByName(contentRoot, "channel");
+
+            res[feedNum] = new string[2][][];
+            res[feedNum][0] = new string[10][];
+
+            res[feedNum][0][(int)feedHeader.Title] = new string[] { GetNodeValueByName(contentRoot, "title") };
+            res[feedNum][0][(int)feedHeader.SubTitle] = new string[] { GetNodeValueByName(contentRoot, "subtitle") };
+            res[feedNum][0][(int)feedHeader.Summary] = new string[] { GetNodeValueByName(contentRoot, "description") };
+            res[feedNum][0][(int)feedHeader.Updated] = new string[] { GetNodeValueByName(contentRoot, "lastBuildDate") };
+            res[feedNum][0][(int)feedHeader.Link] = new string[] { GetNodeValueByName(contentRoot, "link") };
+
+            res[feedNum][0][(int)feedHeader.AuthorName] = new string[] { "" };
+            res[feedNum][0][(int)feedHeader.AuthorUri] = new string[] { "" };
+
+            string[][] entries = new string[udonXml.GetChildNodesCount(contentRoot)][];
+
+            int cnt = 0;
+            for (int i = 0; i < udonXml.GetChildNodesCount(contentRoot); i++)
+            {
+                var entry = udonXml.GetChildNode(contentRoot, i);
+                if (udonXml.GetNodeName(entry) == "item")
+                {
+                    entries[cnt] = new string[6];
+                    entries[cnt][(int)feedEntry.Title] = GetNodeValueByName(entry, "title");
+                    entries[cnt][(int)feedEntry.SubTitle] = GetNodeValueByName(entry, "subtitle");
+                    entries[cnt][(int)feedEntry.Link] = GetNodeValueByName(entry, "link");
+                    entries[cnt][(int)feedEntry.Summary] = GetNodeValueByName(entry, "description");
+                    entries[cnt][(int)feedEntry.Updated] = GetNodeValueByName(entry, "pubDate");
+                    cnt++;
+                }
+            }
+            res[feedNum][1] = new string[cnt][];
+
+            System.Array.Copy(entries, res[feedNum][1], cnt);
+
+            done = true;
+            loadingIttr++;
+        }
+
+        private void parseAtom(int feedNum, object contentRoot)
+        {
+            res[feedNum] = new string[2][][];
+            res[feedNum][0] = new string[10][];
+
+            res[feedNum][0][(int)feedHeader.Title] = new string[] { GetNodeValueByName(contentRoot, "title") };
+            res[feedNum][0][(int)feedHeader.SubTitle] = new string[] { GetNodeValueByName(contentRoot, "subtitle") };
+            res[feedNum][0][(int)feedHeader.Summary] = new string[] { GetNodeValueByName(contentRoot, "summary") };
+            res[feedNum][0][(int)feedHeader.Updated] = new string[] { GetNodeValueByName(contentRoot, "updated") };
+            res[feedNum][0][(int)feedHeader.Link] = new string[] { GetNodeValueByName(contentRoot, "link") };
+
+            var author = udonXml.GetChildNodeByName(contentRoot, "author");
+            res[feedNum][0][(int)feedHeader.AuthorName] = new string[] { GetNodeValueByName(author, "name") };
+            res[feedNum][0][(int)feedHeader.AuthorUri] = new string[] { GetNodeValueByName(author, "name") };
+
+            string[][] entries = new string[udonXml.GetChildNodesCount(contentRoot)][];
+
+            int cnt = 0;
+            for (int i = 0; i < udonXml.GetChildNodesCount(contentRoot); i++)
+            {
+                var entry = udonXml.GetChildNode(contentRoot, i);
+                if (udonXml.GetNodeName(entry) == "entry")
+                {
+                    entries[cnt] = new string[6];
+                    entries[cnt][(int)feedEntry.Title] = GetNodeValueByName(entry, "title");
+                    entries[cnt][(int)feedEntry.SubTitle] = GetNodeValueByName(entry, "subtitle");
+                    entries[cnt][(int)feedEntry.Link] = GetNodeValueByName(entry, "link");
+                    entries[cnt][(int)feedEntry.Summary] = GetNodeValueByName(entry, "summary");
+                    entries[cnt][(int)feedEntry.Updated] = GetNodeValueByName(entry, "updated");
+                    cnt++;
+                }
+            }
+            res[feedNum][1] = new string[cnt][];
+
+            System.Array.Copy(entries, res[feedNum][1], cnt);
+
+            done = true;
+            loadingIttr++;
+        }
+
+        public bool isReady()
+        {
+            return done && loadingIttr >= FeedURL.Length;
+        }
+
+        public int getFeedLength()
+        {
+            return res.Length;
+        }
+
+        public int getFeedEntryLength(int feedNum)
+        {
+            return res[feedNum][1].Length;
+        }
+        public string getFeedEntryItem(int feedNum, int item, feedEntry entry)
+        {
+            Debug.Log(res[feedNum][1].Length);
+            Debug.Log(item);
+            return res[feedNum][1][item][(int)entry];
+        }
+
+        public string getFeedHeaderItem(int feedNum, feedHeader entry)
+        {
+            return res[feedNum][0][(int)entry][0];
+        }
+    }
+
+    public enum feedHeader
+    {
+        Title, SubTitle, Summary, Id, Link, Updated, Rights, Entries, AuthorName, AuthorUri
+    }
+    public enum feedEntry
+    {
+        Title, SubTitle, Summary, Id, Link, Updated
+    }
+}
